5bde57376eb11f7d220400c4ca8f4e538317c251
Mike Bishop <mike@drinklist.co>
2021-03-01 14:10:43 +0000

    Add entry.html and  parsing from markdown2 to HTML

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,4 @@
+import markdown2
 from django.shortcuts import render
 
 from . import util
@@ -6,5 +7,13 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+
+# Entry View
+# Returns the entry of a given title
+def entry(request, title):
+    return render(request, "encyclopedia/entry.html", {
+        "entry": markdown2.markdown(util.get_entry(title))
     })
 
