68f7bf8a76fc0e36ce49a824c921ce3c949752e6
Mike Bishop <mike@drinklist.co>
2021-03-01 17:55:43 +0000

    Add check for empty search string

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,8 @@
 import markdown2
 from django.http import HttpResponse, Http404
-from django import forms
+# from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 
 from . import util
@@ -8,24 +10,30 @@ from . import util
 
 def search(request):
     search_string = request.GET.get("q")
-    print("search string: " + search_string)
-    partial_matches = []
-    for title in util.list_entries():
-        if title == search_string:
-            return render(request, "encyclopedia/entry.html", {
-                "entry": markdown2.markdown(util.get_entry(title))
-        })
-        elif search_string in title:
-            partial_matches += [title]
-    if len(partial_matches) > 0:
-        return render(request, "encyclopedia/search.html", {
-            "entries": partial_matches,
-            "search": search_string
+    if len(search_string) > 0:
+        # print("search string: " + search_string)
+        partial_matches = []
+        for title in util.list_entries():
+            if title == search_string:
+                return render(request, "encyclopedia/entry.html", {
+                    "entry": markdown2.markdown(util.get_entry(title))
+            })
+            elif search_string in title:
+                partial_matches += [title]
+        if len(partial_matches) > 0:
+            return render(request, "encyclopedia/search.html", {
+                "entries": partial_matches,
+                "search": search_string
+            })
+        else:
+            return render(request, "encyclopedia/index.html", {
+                "entries": util.list_entries()
         })
     else:
-        return render(request, "encyclopedia/index.html", {
-            "entries": util.list_entries()
-    })
+        return HttpResponseRedirect(reverse("index"))
+        # return render(request, "encyclopedia/index.html", {
+        #     "entries": util.list_entries()
+        # })
 
 
 def index(request):
