03e840bd0356c8d2772349fc948143fb02554c55
Mike Bishop <mike@drinklist.co>
2021-03-02 12:41:43 +0000

    Add 'Create New Page' & 'Edit Page', link, url, view, html files

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
-    path("wiki/search/", views.search, name="search")
+    path("wiki/search/", views.search, name="search"),
+    path("wiki/new/", views.new, name="new"),
+    path("wiki/<str:title>/edit/", views.edit, name="edit")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,6 @@
 import markdown2
-from django.http import HttpResponse, Http404
 # from django import forms
+from django.http import HttpResponse, Http404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
@@ -13,10 +13,11 @@ def search(request):
     if len(search_string) > 0:
         # print("search string: " + search_string)
         partial_matches = []
-        for title in util.list_entries():
+        for title in util.list_entries(): #if title in ...entries ?
             if title == search_string:
                 return render(request, "encyclopedia/entry.html", {
-                    "entry": markdown2.markdown(util.get_entry(title))
+                    "entry": markdown2.markdown(util.get_entry(title)),
+                    "title": title
             })
             elif search_string in title:
                 partial_matches += [title]
@@ -30,10 +31,7 @@ def search(request):
                 "entries": util.list_entries()
         })
     else:
-        return HttpResponseRedirect(reverse("index"))
-        # return render(request, "encyclopedia/index.html", {
-        #     "entries": util.list_entries()
-        # })
+        return HttpResponseRedirect(reverse("index"))#reverse? but arrives from any page
 
 
 def index(request):
@@ -49,6 +47,17 @@ def entry(request, title):
     if entry == None:
         raise Http404("Entry not found")
     return render(request, "encyclopedia/entry.html", {
-        "entry": markdown2.markdown(entry)
+        "entry": markdown2.markdown(entry),
+        "title": title
+    })
+
+def new(request):
+    return render(request, "encyclopedia/new.html")
+
+def edit(request, title):# title not being passed..
+    entry = util.get_entry(title)
+    return render(request, "encyclopedia/edit.html", {
+        "entry": entry,
+        "title": title
     })
 
