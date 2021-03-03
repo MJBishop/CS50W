832143469162544da90ba6aea6b1deb81ae3f4c3
Mike Bishop <mike@drinklist.co>
2021-03-03 16:04:42 +0000

    Add New Entry, from, style, saving

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -10,15 +10,17 @@ from . import util
 
 #Form Class
 class TextAreaForm(forms.Form):
-    entry = forms.CharField(widget=forms.Textarea, label='')
+    entryField = forms.CharField(widget=forms.Textarea, label='')
+class TitleAndTextAreaForm(forms.Form):
+    titleField = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Title'}), label = '')
+    entryField = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Entry'}), label='')
 
 
 def search(request):
     search_string = request.GET.get("q")
     if len(search_string) > 0:
-        print("search string: " + search_string)
         partial_matches = []
-        for title in util.list_entries(): #if title in ...entries ?
+        for title in util.list_entries():
             if title == search_string:
                 return render(request, "encyclopedia/entry.html", {
                     "entry": markdown2.markdown(util.get_entry(title)),
@@ -36,7 +38,7 @@ def search(request):
                 "entries": util.list_entries()
         })
     else:
-        return HttpResponseRedirect(reverse("encyclopedia:index"))#reverse? but arrives from any page
+        return HttpResponseRedirect(reverse("encyclopedia:index"))
 
 
 def index(request):
@@ -56,25 +58,40 @@ def entry(request, title):
         "title": title
     })
 
-
-def new(request):
-    return render(request, "encyclopedia/new.html")
-
 def edit(request, title):
     if request.method == "POST":
         form = TextAreaForm(request.POST)
         if form.is_valid():
             # Save the edited Entry
-            entry = form.cleaned_data["entry"]
+            entry = form.cleaned_data["entryField"]
             util.save_entry(title, entry)
             # Redirect back to entry page
-            return HttpResponseRedirect(reverse("encyclopedia:entry", args=(title,)))
-        else:
+            # return HttpResponseRedirect(reverse("encyclopedia:entry", args=(title,)))
+        # else:
             # Throw exception?
-            return HttpResponseRedirect(reverse("encyclopedia:entry", args=(title,)))
+        return HttpResponseRedirect(reverse("encyclopedia:entry", args=(title,)))
 
     entry = util.get_entry(title)
     return render(request, "encyclopedia/edit.html", {
         "title": title,
-        "form": TextAreaForm(initial={'entry': entry})
+        "form": TextAreaForm(initial={'entryField': entry})
+    })
+
+def add(request):
+    if request.method == "POST":
+        form = TitleAndTextAreaForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["titleField"]
+            entry = util.get_entry(title)
+            if entry != None:
+                raise Http404("Entry not found")
+            else:
+                entry = form.cleaned_data["entryField"]
+                util.save_entry(title, entry) 
+                return HttpResponseRedirect(reverse("encyclopedia:entry", args=(title,)))
+        return HttpResponseRedirect(reverse("encyclopedia:add"))
+
+    return render(request, "encyclopedia/new.html", {
+        "title": "",
+        "form": TitleAndTextAreaForm()
     })
