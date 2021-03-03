674177588c53ed93cc9add64318eed2a408b34ee
Mike Bishop <mike@drinklist.co>
2021-03-03 14:55:51 +0000

    Edit Entry, using method=POST

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/", views.index, name="index"),
-    path("wiki/<str:title>", views.entry, name="entry"),
+    path("wiki/<str:title>/", views.entry, name="entry"),
+    # path("wiki/<str:title>", views.randomEntry, name="randomEntry"),
     path("wiki/search/", views.search, name="search"),
     path("wiki/new/", views.new, name="new"),
-    path("wiki/<str:title>/edit/", views.edit, name="edit"),
-    path("wiki/<str:title>/save", views.save, name="save")
+    path("wiki/<str:title>/edit/", views.edit, name="edit")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,11 +1,16 @@
 import markdown2
-# from django import forms
+from random import *
+from django import forms
 from django.http import HttpResponse, Http404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
 
 from . import util
+
+#Form Class
+class TextAreaForm(forms.Form):
+    entry = forms.CharField(widget=forms.Textarea, label='')
 
 
 def search(request):
@@ -31,7 +36,7 @@ def search(request):
                 "entries": util.list_entries()
         })
     else:
-        return HttpResponseRedirect(reverse("index"))#reverse? but arrives from any page
+        return HttpResponseRedirect(reverse("encyclopedia:index"))#reverse? but arrives from any page
 
 
 def index(request):
@@ -51,24 +56,25 @@ def entry(request, title):
         "title": title
     })
 
+
 def new(request):
     return render(request, "encyclopedia/new.html")
 
 def edit(request, title):
+    if request.method == "POST":
+        form = TextAreaForm(request.POST)
+        if form.is_valid():
+            # Save the edited Entry
+            entry = form.cleaned_data["entry"]
+            util.save_entry(title, entry)
+            # Redirect back to entry page
+            return HttpResponseRedirect(reverse("encyclopedia:entry", args=(title,)))
+        else:
+            # Throw exception?
+            return HttpResponseRedirect(reverse("encyclopedia:entry", args=(title,)))
+
     entry = util.get_entry(title)
     return render(request, "encyclopedia/edit.html", {
-        "entry": entry,
-        "title": title
-    })
-
-def save(request, title):
-    entry = request.GET.get("entry")
-    print(title)
-    print(entry)
-
-    util.save_entry(title, entry)
-    entry = util.get_entry(title)
-    return render(request, "encyclopedia/entry.html", {
-        "entry": markdown2.markdown(entry),
-        "title": title
+        "title": title,
+        "form": TextAreaForm(initial={'entry': entry})
     })
