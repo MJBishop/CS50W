5c06b6d760ee0cd71b1e415cce0cad9f2cab705f
Mike Bishop <mike@drinklist.co>
2021-03-07 13:20:29 +0000

    Add Model classes: Listing, Bid, Comment. Add admin classes

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import User, Listing, Bid, Comment
 
 # Register your models here.
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("id", "owner", "title", "description", "starting_bid", "img_url", "category")
+    filter_horizontal = ("watching",)
+
+class BidAdmin(admin.ModelAdmin):
+    list_display = ("id", "listing", "user", "bid") 
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "listing", "user", "comment")
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "is_superuser")
+
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Bid, BidAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(User, UserAdmin)
+
--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,28 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MinValueValidator
+from decimal import Decimal
 
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    starting_bid = models.DecimalField(max_digits=7, decimal_places=2, validators=[MinValueValidator(Decimal('0.01'))])
+    img_url = models.URLField(blank=True, default='')
+    category = models.CharField(max_length=30, blank=True, default='')
+    watching = models.ManyToManyField(User, blank=True, related_name="watchlist")
+
+class Bid(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    bid = models.DecimalField(max_digits=7, decimal_places=2)
+
+class Comment(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(User, to_field="username", on_delete=models.CASCADE, related_name="comments")
+    # user_name = models.CharField(max_length=150)
+    comment = models.TextField(max_length=200)
