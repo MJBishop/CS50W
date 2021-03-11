079f0b0663e15dd5e7c959aa1916d38b41ea1687
Mike Bishop <mike@drinklist.co>
2021-03-11 10:53:37 +0000

    Add date_created for Listing, Bid, Comment and active flag for Listing Models

--- auctions/models.py
+++ auctions/models.py
@@ -15,17 +15,17 @@ class Listing(models.Model):
     img_url = models.URLField(blank=True, default='')
     category = models.CharField(max_length=30, blank=True, default='')
     watching = models.ManyToManyField(User, blank=True, related_name="watchlist")
-    # date_created = models.DateTimeField(auto_now_add=True)
-    # active = models.BooleanField(default=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
 
 class Bid(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
     bid = models.DecimalField(max_digits=7, decimal_places=2)
-    # date_created = models.DateTimeField(auto_now_add=True)
+    date_created = models.DateTimeField(auto_now_add=True)
 
 class Comment(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
     user_name = models.ForeignKey(User, to_field="username", on_delete=models.CASCADE, related_name="comments")
     comment = models.TextField(max_length=200)
-    # date_created = models.DateTimeField(auto_now_add=True)
+    date_created = models.DateTimeField(auto_now_add=True)
