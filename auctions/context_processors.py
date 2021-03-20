c62e60bd55e0062f34f8e1e14fe118ea7f60e4db
Mike Bishop <mike@drinklist.co>
2021-03-20 13:47:55 +0000

    Remove count of Categories from context prosessor

--- auctions/context_processors.py
+++ auctions/context_processors.py
@@ -4,8 +4,8 @@ def add_listing_counts_to_context(request):
     active_listings = Listing.objects.filter(active=True)
     return {
         'active_listings_count': active_listings.count(),
-        'categories_count': active_listings.values('category')
-                                           .distinct()
-                                           .exclude(category__exact='')
-                                           .count()
+        # 'categories_count': active_listings.values('category')
+        #                                    .distinct()
+        #                                    .exclude(category__exact='')
+        #                                    .count()
     }
