f7d79c918dfa6a873cfb7a80a821553f2f2ae6d0
Mike Bishop <mike@drinklist.co>
2021-05-21 15:11:03 +0100

    Add FollowManager, create_follow, delete_follow, test_create_follow and test_delete_follow Pass

--- network/models.py
+++ network/models.py
@@ -5,9 +5,21 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+class FollowManager(models.Manager):
+    def create_follow(self, from_user, to_user):
+        follow = Follow(from_user=from_user, to_user=to_user)
+        follow.save()
+        return follow
+
+    def delete_follow(self, from_user, to_user):
+        unfollow = self.filter(from_user=from_user, to_user=to_user)
+        unfollow.delete()
+
 class Follow(models.Model):
     from_user = models.ForeignKey(User, editable=False, on_delete=models.CASCADE, related_name='following')
     to_user = models.ForeignKey(User, editable=False, on_delete=models.CASCADE, related_name='followers')
+
+    objects = FollowManager()
 
 class Post(models.Model):
     user = models.ForeignKey(User, editable=False, on_delete=models.CASCADE, related_name='posts')
--- network/tests.py
+++ network/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
 
+from .models import User, Follow, Post
+
 # Create your tests here.
+class NetworkModelTestCase(TestCase):
+
+    def setUp(self):
+
+        # create Users
+        user1 = User.objects.create_user('Mike')
+        user2 = User.objects.create_user('James')
+
+        # create Follow
+        f1 = Follow.objects.create(from_user=user1, to_user=user2)
+
+    # Follow tests
+    def test_follower_count_zero(self):
+        u1 = User.objects.get(username='Mike')
+        self.assertEqual(u1.followers.count(), 0)
+    
+    def test_follower_count_one(self):
+        u2 = User.objects.get(username='James')
+        self.assertEqual(u2.followers.count(), 1)
+        
+    def test_create_follow(self):
+        u1 = User.objects.get(username='Mike')
+        u2 = User.objects.get(username='James')
+        follow = Follow.objects.create_follow(u2, u1)
+        self.assertEqual(u1.followers.count(), 1)
+
+    def test_delete_follow(self):
+        u1 = User.objects.get(username='Mike')
+        u2 = User.objects.get(username='James')
+        follow = Follow.objects.delete_follow(u1, u2)
+        self.assertEqual(u2.followers.count(), 0)
+
