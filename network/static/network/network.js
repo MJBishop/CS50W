1bcf9723c8800b457c4c4565749dbd6abe5af280
Mike Bishop <mike@drinklist.co>
2021-07-02 17:01:17 +0100

    Add network.js

---
+++ network/static/network/network.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    console.log('DOM ready')
+});
+
+document.querySelector('#post-list-view').addEventListener('DOMContentLoaded', function() {
+
+    console.log('post-list-view ready')
+});
+  
