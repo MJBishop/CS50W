0dacbd8ff01d776aec0b26098ccbb6a40a0693a3
Mike Bishop <mike@drinklist.co>
2022-01-15 13:29:35 +0800

    Admin model classes

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import User, Post, Follow
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "date_created", "text")
+    filter_horizontal = ("likes",)
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ("id", "from_user", "to_user")
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "is_superuser")
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Follow, FollowAdmin)
+admin.site.register(User, UserAdmin)
