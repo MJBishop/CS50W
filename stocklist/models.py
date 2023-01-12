9df1c3a68593d132ec44f378d4ce5572bafa1190
Mike Bishop <mike@drinklist.co>
2023-01-12 12:41:22 +0000

    Pass test_create_store

--- stocklist/models.py
+++ stocklist/models.py
@@ -3,5 +3,11 @@ from django.db import models
 
 # Create your models here.
 
+MAX_STORE_NAME_LENGTH = 20
+
 class User(AbstractUser):
     pass
+
+class Store(models.Model):
+    owner = models.ForeignKey(User, editable=False, on_delete=models.CASCADE, related_name="stores")
+    name = models.CharField(max_length=MAX_STORE_NAME_LENGTH)
--- stocklist/tests/test_models.py
+++ stocklist/tests/test_models.py
@@ -2,7 +2,8 @@ import json
 from django.test import Client, TestCase
 from django.core.exceptions import ValidationError
 
-from stocklist.models import User
+from stocklist.models import User, Store
+
 
 class UserTestCase(TestCase):
 
@@ -14,9 +15,26 @@ class UserTestCase(TestCase):
 
         return super().setUpTestData()
     
-    # User tests
     def test_user(self):
-
         users = User.objects.all()
         self.assertEqual(users.count(), 1)
         self.assertEqual(users[0].username, 'Mike')
+
+
+class StoreTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+
+        # create User
+        cls.user1 = User.objects.create_user('Mike')
+        cls.store_name = "Test Store"
+
+        return super().setUpTestData()
+
+    def test_create_store(self):
+        store = Store.objects.create(owner=self.user1, name=self.store_name)
+
+        stores = Store.objects.all()
+        self.assertEqual(stores.count(), 1)
+        self.assertEqual(stores[0].name, self.store_name)
